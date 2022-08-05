3954db1cdd15823c6105f46429659bb758768fef
rakesh-pilly <rakesh.mudhiraj882@gmail.com>
2022-08-05 13:20:07 +0530

    update for heroku

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ import cors from "cors"
 // FUH68ngULGGyYJD0
 // mongodb+srv://admin:<password>@cluster0.4dpcr.mongodb.net/?retryWrites=true&w=majority
 const app = express();
-
+const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
@@ -22,9 +22,9 @@ mongoose
     "mongodb+srv://admin:FUH68ngULGGyYJD0@cluster0.4dpcr.mongodb.net/Blog?retryWrites=true&w=majority"
   )
   .then(() => {
-    app.listen(5000);
+    app.listen(port);
   })
-  .then(()=> console.log("connected to Database and listeting to localhost"))
+  .then(()=> console.log("connected to Database and listeting to localhost" + port))
   .catch((err)=> console.log(err))
 
 
